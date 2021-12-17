0848baf42f0f9fbb86a6e84ed3608ce669f30dac
siksaka09 <earthsik09@gmail.com>
2021-12-17 18:05:08 +0700

    lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float mean = 0;
+    float standevi = 0;
+    float sum;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+        mean += atof(textline.c_str());
+        standevi += pow(atof(textline.c_str()),2);
+        count++;
+    }
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    sum = mean/count;
+    cout << "Mean = " << sum << "\n";
+    cout << "Standard deviation = " << sqrt((standevi/count) - pow(sum,2));
+    source.close();
+    return 0;
+}
